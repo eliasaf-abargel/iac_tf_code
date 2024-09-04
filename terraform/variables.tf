aabc2c1ed6951c08bb901d8e0a0d563270c65c0f
eliasaf-abargel <eliasafabargel@gmail.com>
2024-09-04 22:53:32 +0300

    "updated terraform files"

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -44,7 +44,7 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0c7217cdde317cfec" // Replace with the latest AMI ID for your region
+  default     = "ami-0e86e20dae9224db8" # Ubuntu 20.04
 }
 
 variable "instance_type" {
@@ -56,7 +56,7 @@ variable "instance_type" {
 variable "key_name" {
   description = "EC2 keypair"
   type        = string
-  default     = "gitopskey"
+  default     = "key_mac3"
 }
 
 variable "instance_name" {
